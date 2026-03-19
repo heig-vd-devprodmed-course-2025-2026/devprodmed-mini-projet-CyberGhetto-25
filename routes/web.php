6ca4f2d4bbc8fd231e10bd322192497edb0ddc4b
Marc Bridy <marc.bridy@heig-vd.ch>
2026-03-19 14:57:29 +0100

    Mettre en place le formulaire pour liker un post

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\LikeController;
 
 Route::get('/', function () {
     $posts = Post::orderBy('created_at', 'desc')->with('user')->with('likes')->limit(3)->get();
@@ -18,3 +19,5 @@ Route::get('/about', function () {
 Route::get('/@{username}', [ProfileController::class, 'show'])->where('username', '[A-Za-z0-9-_]+');
 
 Route::resource('posts', PostController::class);
+
+Route::match(['put', 'patch'], '/likes/{post}', [LikeController::class, 'update']);
